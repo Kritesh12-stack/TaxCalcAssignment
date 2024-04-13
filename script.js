aeeb331c26ca05861fc05607e03e534a83a35407
Kritesh-Shrivastava <kriteshshri@gmail.com>
2024-04-13 10:39:41 +0530

    basic ui development of form according to ui

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let ageOption1 = document.querySelector("#ageOption1");
 let ageOption2 = document.querySelector("#ageOption2");
 let ageOption3 = document.querySelector("#ageOption3");
 
+
+
 ageSelectorBtn.addEventListener("click", (e) => {
   e.preventDefault();
   let displaySetting = ageOptions.style.display;
@@ -43,10 +45,55 @@ ageSelectorBtn.addEventListener("click", (e) => {
 
 ageOption1.addEventListener("click", () => {
   ageSelectInput.value = ageOption1.textContent;
+  ageOptions.style.display = "none";
+  ageSelectorBtn.innerHTML = `<svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>`;
 });
 ageOption2.addEventListener("click", () => {
   ageSelectInput.value = ageOption2.textContent;
+  ageOptions.style.display = "none";
+  ageSelectorBtn.innerHTML = `<svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>`;
 });
 ageOption3.addEventListener("click", () => {
   ageSelectInput.value = ageOption3.textContent;
+  ageOptions.style.display = "none";
+  ageSelectorBtn.innerHTML = `<svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>`;
 });
