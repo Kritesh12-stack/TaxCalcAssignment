57910a5c5c92cca5dd294d6441b695ec911246a7
Kritesh-Shrivastava <kriteshshri@gmail.com>
2024-04-14 08:31:35 +0530

    Completed Error Handling in Form

--- script.js
+++ script.js
@@ -1,9 +1,69 @@
-let ageSelectorBtn = document.querySelector(".dropdown-select-btn");
-let ageSelectInput = document.querySelector(".dropdown-select");
-let ageOptions = document.querySelector(".dropdown-menu");
-let ageOption1 = document.querySelector("#ageOption1");
-let ageOption2 = document.querySelector("#ageOption2");
-let ageOption3 = document.querySelector("#ageOption3");
+const submitBtn = document.querySelector(".submitBtn");
+const ageSelectorBtn = document.querySelector(".dropdown-select-btn");
+const ageSelectInput = document.querySelector(".dropdown-select");
+const ageOptions = document.querySelector(".dropdown-menu");
+const ageOption1 = document.querySelector("#ageOption1");
+const ageOption2 = document.querySelector("#ageOption2");
+const ageOption3 = document.querySelector("#ageOption3");
+
+submitBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let errorMsgs = {};
+    const annualIncome = document.querySelector("#grossIncome");
+    const grossIncomeError = document.querySelector(".grossIncomeError");
+    const extraIncome = document.querySelector("#extraIncome");
+    const extraIncomeError = document.querySelector(".extraIncomeError");
+    const ageGroup = document.querySelector("#ageGroup");
+    const ageGroupError = document.querySelector(".ageGroupError");
+    const deductions = document.querySelector("#deductions");
+    const deductionsError = document.querySelector(".deductionsError")
+    if(annualIncome.value === 0 || annualIncome.value === ''){
+        errorMsgs = {...errorMsgs,incomeError : "Annual Income Is Required"}
+    }
+    if (extraIncome.value === '' || extraIncome.value === 0){
+        extraIncome.value = 0
+    }
+    if (ageGroup.value === ''){
+      errorMsgs = {...errorMsgs,ageError : "Age Group is Required"}
+    }
+    if(deductions.value === '' || deductions.value === 0){
+      deductions.value = 0
+    }
+    else if(deductions.value > annualIncome.value || deductions.value === annualIncome.value){
+      errorMsgs = {...errorMsgs,deductionError : "Deductions Cannot be greater than or equal to Annual Income"}
+    }
+    if(Object.keys(errorMsgs).length > 0){
+      if(errorMsgs.hasOwnProperty('incomeError') && errorMsgs.incomeError !== ''){
+        grossIncomeError.style.display = 'block';
+        grossIncomeError.textContent = errorMsgs['incomeError']
+      }
+      if(errorMsgs.hasOwnProperty('ageError') && errorMsgs.ageError !== ''){
+        ageGroupError.style.display = 'block';
+        ageGroupError.textContent = errorMsgs['ageError']
+      }
+      if(errorMsgs.hasOwnProperty('deductionError') && errorMsgs.deductionError !== ''){
+        deductionsError.style.display = 'block';
+        deductionsError.textContent = errorMsgs['deductionError']
+      }
+    }
+    if(Object.keys(errorMsgs).length === 0 ){
+      errorMsgs.incomeError = ''
+      errorMsgs.ageError = ''
+      errorMsgs.deductionError = ''
+      grossIncomeError.style.display = "none";
+      grossIncomeError.textContent = ''
+      ageGroupError.style.display = "none";
+      ageGroupError.textContent = ''
+      deductionsError.style.display = "none";
+      deductionsError.textContent = ''
+
+      
+    }
+    
+
+    
+
+})
 
 
 
@@ -97,3 +157,5 @@ ageOption3.addEventListener("click", () => {
               />
             </svg>`;
 });
+
+
